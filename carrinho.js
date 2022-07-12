854528dcea36e13410faeff3fa949cb202614f0d
Wesley de Matos Souza <wesleydematos3@gmail.com>
2022-07-11 21:58:58 -0300

    carrinho

---
+++ carrinho.js
@@ -0,0 +1,6 @@
+let sectionCarrinho = document.createElement("section")
+let tituloCarrinho = document.createElement("h2")
+tituloCarrinho.innerHTML = "🛒 Carrinho"
+sectionCarrinho.id = "carrinho"
+sectionCarrinho.appendChild(tituloCarrinho)
+corpoSite.appendChild(sectionCarrinho)
